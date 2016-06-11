00f588cbe6b21bee204788d424e1347c4ccd2b02
sesta <sesta.ec5@gmail.com>
2016-06-12 00:43:37 +0900

    add devise_token culumn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611093205) do
+ActiveRecord::Schema.define(version: 20160611153743) do
 
   create_table "bursts", force: :cascade do |t|
     t.integer  "child_id",   limit: 4, null: false
@@ -22,11 +22,12 @@ ActiveRecord::Schema.define(version: 20160611093205) do
   add_index "bursts", ["child_id"], name: "index_bursts_on_child_id", using: :btree
 
   create_table "children", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "parent_id",  limit: 4,                                   null: false
-    t.datetime "created_at",                                             null: false
-    t.datetime "updated_at",                                             null: false
-    t.time     "calm_time",              default: '2000-01-01 12:00:00', null: false
+    t.string   "name",         limit: 255
+    t.integer  "parent_id",    limit: 4,                                   null: false
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
+    t.time     "calm_time",                default: '2000-01-01 12:00:00', null: false
+    t.string   "devise_token", limit: 255,                                 null: false
   end
 
   add_index "children", ["parent_id"], name: "index_children_on_parent_id", using: :btree
